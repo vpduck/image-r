39920c11b6f52c605504ddbe9d5fa9919d75dc9f
vpduck <84106586+vpduck@users.noreply.github.com>
2021-12-03 18:28:47 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+
+    });
+}
+
+
+
+classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/D8v7KO_1G/model.json',modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error){
+        console.error(error);
+
+    } else {
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+
+}
